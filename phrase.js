88bdf86c23f3af7b1956a641a245297023f4b293
marouane <marwanmakhloufraja@gmail.com>
2024-06-27 09:34:37 +0100

    setup files and install tailwind

---
+++ phrase.js
@@ -0,0 +1,90 @@
+const sentences = [
+    {
+        fr: "Comment t'appelles-tu à la gare ?",
+        ar: "كيف تسمى في المحطة؟"
+    },
+    {
+        fr: "Le garçon mange une glace.",
+        ar: "الصبي يأكل آيس كريم."
+    },
+    {
+        fr: "Tu parles bien français, juste un peu.",
+        ar: "أنت تتحدث الفرنسية جيداً، فقط قليلاً."
+    },
+    {
+        fr: "J'ai une gomme à la gare.",
+        ar: "لدي ممحاة في المحطة."
+    },
+    {
+        fr: "Le guide est très grand.",
+        ar: "المرشد طويل جداً."
+    },
+    {
+        fr: "Viens avec moi, c'est grave.",
+        ar: "تعال معي، الأمر خطير."
+    },
+    {
+        fr: "Je viens de marquer un goal.",
+        ar: "لقد سجلت هدفاً للتو."
+    },
+    {
+        fr: "Comment s'appelle le garçon ?",
+        ar: "ما اسم الصبي؟"
+    },
+    {
+        fr: "Je m'appelle Pierre, et toi ?",
+        ar: "أنا اسمي بيير، وأنت؟"
+    },
+    {
+        fr: "Où est la gare ?",
+        ar: "أين المحطة؟"
+    },
+    {
+        fr: "La fille lit un livre.",
+        ar: "البنت تقرأ كتاباً."
+    },
+    {
+        fr: "Le chat dort sur le tableau.",
+        ar: "القطة تنام على السبورة."
+    },
+    {
+        fr: "Le chien joue avec le garçon.",
+        ar: "الكلب يلعب مع الصبي."
+    },
+    {
+        fr: "Le garçon et la fille regardent le tableau.",
+        ar: "الصبي والبنت ينظران إلى السبورة."
+    },
+    {
+        fr: "Le livre est sur le tableau.",
+        ar: "الكتاب على السبورة."
+    },
+    {
+        fr: "Vous désirez une chambre ?",
+        ar: "تحت أمرك - أتفضل غرفة؟"
+    },
+    {
+        fr: "Je voudrais une chambre.",
+        ar: "أريد غرفة."
+    },
+    {
+        fr: "C'est combien ?",
+        ar: "بكم؟"
+    },
+    {
+        fr: "Avec bains ou avec douche ?",
+        ar: "بانيو أو بالدش؟"
+    },
+    {
+        fr: "Pour combien de nuits ?",
+        ar: "لكم ليلة؟"
+    },
+    {
+        fr: "Pour combien de jours ?",
+        ar: "لكم يوم؟"
+    },
+    {
+        fr: "Pour combien de personnes ?",
+        ar: "لكم شخص؟"
+    }
+];
